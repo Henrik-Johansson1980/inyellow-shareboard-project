2189da67237a4d436a699a8560cf586af129654c
Henrik Johansson <henrikjohansson1980@gmail.com>
2020-05-17 13:10:24 +0200

    HerokuTest

--- config.php
+++ config.php
@@ -6,5 +6,5 @@ define("DB_PASS", "b50a55c1");
 define("DB_NAME", "heroku_56305218cb2a835");
 
 //Define URLS
-define("ROOT_PATH", "in-yellow-shareboard.herokuapp.com/");
+define("ROOT_PATH", "/");
 define("ROOT_URL", "https://in-yellow-shareboard.herokuapp.com/");
