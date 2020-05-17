13d9a918d91149ab21aa316c1e2fdce6fbfb36c1
Henrik Johansson <henrikjohansson1980@gmail.com>
2020-05-17 13:36:50 +0200

    HerokuTest

--- config.php
+++ config.php
@@ -7,6 +7,6 @@ define("DB_NAME", "heroku_56305218cb2a835");
 
 //Define URLS
 define("ROOT_PATH", "/");
-define("ROOT_URL", "/");
+define("ROOT_URL", "https://in-yellow-shareboard.herokuapp.com/");
 
 //mysql://b30e613852906c:b50a55c1@eu-cdbr-west-03.cleardb.net/heroku_56305218cb2a835?reconnect=true
