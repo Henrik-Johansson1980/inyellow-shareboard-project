6b3df254afdfec689c143bce0da5c8b66be0f547
Henrik Johansson <henrikjohansson1980@gmail.com>
2020-05-17 13:32:10 +0200

    HerokuTest

--- config.php
+++ config.php
@@ -9,4 +9,4 @@ define("DB_NAME", "heroku_56305218cb2a835");
 define("ROOT_PATH", "/");
 define("ROOT_URL", "/");
 
-mysql://b30e613852906c:b50a55c1@eu-cdbr-west-03.cleardb.net/heroku_56305218cb2a835?reconnect=true
+//mysql://b30e613852906c:b50a55c1@eu-cdbr-west-03.cleardb.net/heroku_56305218cb2a835?reconnect=true
