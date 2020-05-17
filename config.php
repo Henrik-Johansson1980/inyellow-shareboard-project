6644dcd8b026aec6683b782f0063cc30a5aa88bb
Henrik Johansson <henrikjohansson1980@gmail.com>
2020-05-17 12:25:04 +0200

    Final Commit

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 //DB PArams
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "shareboard");
+define("DB_HOST", "eu-cdbr-west-03.cleardb.net");
+define("DB_USER", "b30e613852906c");
+define("DB_PASS", "b50a55c1");
+define("DB_NAME", "`heroku_56305218cb2a835`");
 
 //Define URLS
-define("ROOT_PATH", "/php-oop/inyellowshareboard/");
-define("ROOT_URL", "http://localhost/php-oop/inyellowshareboard/");
+define("ROOT_PATH", "/");
+define("ROOT_URL", "https://in-yellow-shareboard.herokuapp.com/");
  
