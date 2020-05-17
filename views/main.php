eaf19e4c270fff716398c87232b7ad169dbdcf07
Henrik Johansson <henrikjohansson1980@gmail.com>
2020-05-17 14:26:10 +0200

    HerokuTest

--- views/main.php
+++ views/main.php
@@ -42,7 +42,7 @@
       <?php if(isset($_SESSION['error_message']) OR isset($_SESSION['success_message'])) :?>
         <?php Message::display(); ?>
       <?php endif;?>
-     	<?php var_dump($view);//require($view); ?>
+     	<?php var_dump($view); require($view); ?>
      </div>
     </div><!-- /.container -->
   <footer>
