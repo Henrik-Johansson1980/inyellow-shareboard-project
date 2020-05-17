06a086ed476f8e39d7f965641b9b9f534922faca
Henrik Johansson <henrikjohansson1980@gmail.com>
2020-05-17 14:55:08 +0200

    Final Commit 2020-05-17

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+//DB PArams
+define("DB_HOST", "");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
+
+//Define URLS
+define("ROOT_PATH", "/");
+define("ROOT_URL", "/");
