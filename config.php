a7251b5542e6000f89993c33825b100ec974520c
Henrik Johansson <henrikjohansson1980@gmail.com>
2020-05-17 13:31:32 +0200

    HerokuTest

--- config.php
+++ config.php
@@ -2,9 +2,11 @@
 //DB PArams
 define("DB_HOST", "eu-cdbr-west-03.cleardb.net");
 define("DB_USER", "b30e613852906c");
-define("DB_PASS", "b50a55c1");
+define("DB_PASS", "");
 define("DB_NAME", "heroku_56305218cb2a835");
 
 //Define URLS
 define("ROOT_PATH", "/");
 define("ROOT_URL", "/");
+
+mysql://b30e613852906c:b50a55c1@eu-cdbr-west-03.cleardb.net/heroku_56305218cb2a835?reconnect=true
